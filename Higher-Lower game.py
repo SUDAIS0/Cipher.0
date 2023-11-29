320751e01e22657d4e9d246ebf8c5467ac10a015
Sudais <sudais124578@gmail.com>
2023-11-30 01:28:06 +0500

    Higher-Lower game
    In this game, you will be given two famous personalities, and you have to guess who has the most followers on Instagram. You will be given 10 points for each right guess. (You will have only one chance to get it right.)

---
+++ Higher-Lower game.py
@@ -0,0 +1,387 @@
+logo = """
+           /\  /\ \_   \ / _ \ /\  /\ /__\/__\         / /   /___\/ / /\ \ \ /__\/__\ 
+          / /_/ /  / /\// /_\// /_/ //_\ / \// _____  / /   //  //\ \/  \/ //_\ / \// 
+         / __  //\/ /_ / /_/// __  ///__/ _  \|_____|/ /___/ \_//  \  /\  ///__/ _  \ 
+         \/ /_/ \____/ \____/\/ /_/ \____/ \_/       \____/____/    \/  \/ \__/\/ \_/
+                                     / _ \ /_\    /\/\    /__\                        
+                                    / /_\///_//  /    \  /_\                          
+                                   / /_///  _  \/ /\/\ \//__                          
+                                   \____/\_/ \_/\/    \/\__/                                         
+"""
+logo2 = """
+            /\   /\/ _\   
+            \ \ / /\ \    
+             \ V / _\ \   
+              \_/  \__/   
+"""
+infoList = [{
+    'name' : 'Cristiano Ronaldo',
+    "Profession" : "Footballer",
+    "country" : "Portugal",
+    "followers" : 613 
+},
+{
+    "name" : "Lionel Messi",
+    "Profession" : "Footballer",
+    "country" : "Argentina",
+    "followers" :493
+},
+{
+    "name" : "Selena Gomez",
+    "Profession" : "Musician",
+    "country" : "United States",
+    "followers" : 430
+},
+{
+    "name" : "Kylie Jenner",
+    "Profession" :  "Media Personality",
+    "country" : "United States",
+    "followers" : 398
+},
+{
+    "name" : "Dwayne Johnson",
+    "Profession" : "Actor & Proffesional wrestler",
+    "country" : "United States",
+    "followers" :  392
+},
+{
+    "name" : "Ariana Grande",
+    "Profession" : "Musician",
+    "country" : "United States",
+    "followers" : 381
+},
+{
+    "name" : "Kim Kardashian",
+    "Profession" : "Media Personality",
+    "country" : "United States",
+    "followers" : 364
+},
+{
+    "name" : "Beyoncé",
+    "Profession" : "Musician",
+    "country" :  "United States", 
+    "followers" : 318
+},
+{
+    "name" : "Khloé Kardashian",
+    "Profession" : "Media Personality",
+    "country" : "United States",
+    "followers" : 311
+},
+{
+    "name" : "Nike",
+    "Profession" : "Sport wear multinational",
+    "country" : "United States",
+    "followers" : 306
+},
+{
+    "name" : "Kendall Jenner",
+    "Profession" : "Media Personality",
+    "country" : "United States",
+    "followers" : 294
+},
+{
+    "name" : "Justin Bieber",
+    "Profession" : "Musician",
+    "country" : "Canada",
+    "followers" : 292
+},
+{
+    "name" : "National Geographic",
+    "Profession" : "Magazine",
+    "country" :  "United States",
+    "followers" : 283
+},
+{
+    "name" : "Taylor Swift",
+    "Profession" : "Musician",
+    "country" : "United States",
+    "followers" : 277
+},
+{
+    "name" : "Virat Kohli",
+    "Profession" : "Cricketer",
+    "country" :  "India",
+    "followers" : 262
+},
+{
+    "name" : "Jennifer Lopez",
+    "Profession" : "Musician",
+    "country" : "United States",
+    "followers" : 252
+},
+{
+    "name" : "Nicki Minaj",
+    "Profession" : "Musician",
+    "country" :  "United States",
+    "followers" : 228
+},
+{
+    "name" : "Kourtney Kardashian",
+    "Profession" : "Media Personality",
+    "country" : "United States",
+    "followers" : 224
+},
+{
+    "name" : "Neymar",
+    "Profession" : "Footballer",
+    "country" : "Brazil",
+    "followers" : 216
+},
+{
+    "name" : "Miley Cyrus",
+    "Profession" : "Musician",
+    "country" : "United States",
+    "followers" : 215
+},
+{
+    "name" : "Katy Perry",
+    "Profession" : "Musician",
+    "country" : "United States",
+    "followers" : 206
+},
+{
+    "name" : "Zendaya",
+    "Profession" : "Actress & Singer",
+    "country" : "United States",
+    "followers" : 185
+},
+{
+    "name" : "Kevin Hart",
+    "Profession" : "Comedian & Actor",
+    "country" : "United States",
+    "followers" : 179
+},
+{
+    "name" : "Cardi B",
+    "Profession" : "Musician",
+    "country" : "United States",
+    "followers" : 169
+},
+{
+    "name" : "LeBron James",
+    "Profession" : "Bastketball Player",
+    "country" : "United States",
+    "followers" : 158
+},
+{
+    "name" : "Demi Lovato",
+    "Profession" : "Musician",
+    "country" : "United States",
+    "followers" : 157
+},
+{
+    "name" : "Rihanna",
+    "Profession" : "Musician",
+    "country" : "Barbados",
+    "followers" : 152
+},
+{
+    "name" : "Real Madrid",
+    "Profession" : "Football Club",
+    "country" : "Spain",
+    "followers" : 149
+},
+{
+    "name" : "Chris Brown",
+    "Profession" : "Musician",
+    "country" : "United States",
+    "followers" : 145
+},
+{
+    "name" : "Drake",
+    "Profession" : "Musician",
+    "country" : "Canada",
+    "followers" : 144
+},
+{
+    "name" : "Ellen DeGeneres",
+    "Profession" : "Comidian & Televison Host",
+    "country" : "United States",
+    "followers" : 139
+},
+{
+    "name" : "Barcelona",
+    "Profession" : "Fooball Club",
+    "country" : "Spain",
+    "followers" : 124
+},
+{
+    "name" : "UEFA Champions League",
+    "Profession" : "Football Club Compition League",
+    "country" : "Europe",
+    "followers" : 111
+},
+{
+    "name" : "Billie Eilish",
+    "Profession" : "Musician",
+    "country" : "United States",
+    "followers" : 110
+},
+{
+    "name" : "Kylian Mbappé",
+    "Profession" : "Footballer",
+    "country" : "France",
+    "followers" : 109
+},
+{
+    "name" : "Vin Diesel",
+    "Profession" : "Actor",
+    "country" : "United States",
+    "followers" : 101
+},
+{
+    "name" : "Lisa",
+    "Profession" : "Musicain",
+    "country" : "Thailand",
+    "followers" : 98
+},
+{
+    "name" : "NASA",
+    "Profession" : "Space agency",
+    "country" : "United States",
+    "followers" : 96
+},
+{
+    "name" : "Priyanka Chopra",
+    "Profession" : "Actress",
+    "country" : "India",
+    "followers" : 90
+},
+{
+    "name" : "Shakira",
+    "Profession" : "Musician",
+    "country" : "Colombia",
+    "followers" : 89
+},
+{
+    "name" : "Dua Lipa",
+    "Profession" : "Musicain",
+    "country" : "United Kingdom",
+    "followers" : 88
+},
+{
+    "name" : "David Beckham",
+    "Profession" : "Footballer",
+    "country" : "United Kingdom",
+    "followers" : 85
+},
+{
+    "name" : "Shraddha Kapoor",
+    "Profession" : "Actress",
+    "country" : "India",
+    "followers" : 84
+},
+{
+    "name" : "NBA",
+    "Profession" : "Basketball League",
+    "country" : "United States",
+    "followers" : 83
+},
+{
+    "name" : "Snoop Dogg",
+    "Profession" : "Musicain",
+    "country" : "United States",
+    "followers" : 82
+},
+{
+    "name" : "Jennie",
+    "Profession" : "Musician",
+    "country" : "South korea",
+    "followers" : 81
+},
+{
+    "name" : "Narendra Modi",
+    "Profession" : "Prime Minister of India",
+    "country" : "India",
+    "followers" : 80
+},
+{
+    "name" : "Khaby Lame",
+    "Profession" : "Media Personality",
+    "country" : "Italy & Senegal",
+    "followers" : 79
+},
+{
+    "name" : "Sudais",
+    "Profession" : "Student",
+    "country" : "Pakistan",
+    "followers" : 1
+}
+]
+
+print(logo)
+
+import random
+flag = False
+points = 0
+person2 = infoList[0]
+def persons():
+    global person2
+    if flag :
+        person1 = person2
+        person2 = random.choice(infoList)
+        while person1 == person2 :
+            person2 = random.choice(infoList)
+    else:
+        person1 = random.choice(infoList)
+        person2 = random.choice(infoList)
+        while person1 == person2 :
+            person2 = random.choice(infoList)
+    return person1 , person2
+
+def display():
+    global points
+    person1 , person2 = persons()
+    flags = True
+    while flags :
+        print("\n\t Who has more followers on instagram")
+        print(f"\n\t\t Current points = {points}")
+        print(f"\n\tA: Name: {person1["name"]}\n\t   Profession: {person1["Profession"]}\n\t   Country: {person1["country"]} \n{logo2}\n\tB: Name: {person2["name"]}\n\t   Profession: {person2["Profession"]}\n\t   Country: {person2["country"]}")
+        answer = input("\n\n\t ==>>").lower()
+        if(answer == "a") or (answer == "b"):
+            return answer , person1 , person2
+        else:
+            print("\n\t Error \n\t Invalid Answer")
+
+def RightAnswer():
+    answer , person1 , person2 = display()
+    rightAnswer = 0
+    if (person1["followers"] > person2["followers"]) :
+        rightAnswer = "a"
+    elif (person1["followers"] < person2["followers"]) :
+        rightAnswer = "b"
+
+    return answer , rightAnswer
+
+def check():
+    answer , rightAnswer = RightAnswer()
+
+    if (answer == rightAnswer):
+        global flag
+        print("\n\t Great \n\t You guessed it RIGHT")
+        flag = True
+        return 
+    else: 
+        print("\n\t Oops! \n\t You guessed it WRONG")
+        flag = False
+        return
+
+def main():
+    global flag
+    global points
+    flagg = True
+    while flagg :
+        choice = input("\n\t You want to play the game (yes/no)= ").lower()
+        if choice == "yes" :
+            check()
+            while flag :
+                points += 10
+                check()
+            print(f"\n\t\t Your final points are = {points}")
+        elif choice == "no" :
+            flagg = False
+        else:
+            print("\n\t Error \n\t Invalid Answer")
+
+main()
